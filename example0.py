7ee228f861c229a7f88372852ff7337cb80dfb97
Dave Dutcher <dave.dutcher@telventdtn.com>
2015-10-07 08:49:31 -0500

    Building outline and adding more slides.

---
+++ example0.py
@@ -0,0 +1,18 @@
+import sys
+
+def add(x, y):
+    return x + y
+
+def one():
+    print add(1, 2)
+
+def two():
+    print add(2, 3)
+
+def main():
+    one()
+    two()
+
+
+if __name__ == '__main__':
+    main()
